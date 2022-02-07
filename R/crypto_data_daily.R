477866343b68a61911dbcf6a7e8aa5fc30dbc4ed
v|v <viv.datascientist@gmail.com>
2022-02-07 13:20:15 +0100

    Initial Commit (2022_02_07)
    Update data, documentation und checks for CRAN Submission (all pass)

---
+++ R/crypto_data_daily.R
@@ -0,0 +1,9 @@
+#' Cryptocurrency Data (Daily)
+#'
+#' Times-Series Data for 4 Cryptocurrencies. Bitcoin (BTC), Cardano (ADA), Ethereum (ETH) and Ripple(XRP) from 2018 until 2022.
+#'
+#' @format A dataframe with 5'801 rows and 11 variables.
+#'
+#' @examples
+#' crypto_data_daily
+"crypto_data_daily"
---
+++ R/crypto_data_hourly.R
@@ -0,0 +1,9 @@
+#' Cryptocurrency Data (Hourly)
+#'
+#' Times-Series Data for 4 Cryptocurrencies. Bitcoin (BTC), Cardano (ADA), Ethereum (ETH) and Ripple(XRP) from 2018 until 2022.
+#'
+#' @format A dataframe with 19'847 rows and 11 variables.
+#'
+#' @examples
+#' crypto_data_hourly
+"crypto_data_hourly"
